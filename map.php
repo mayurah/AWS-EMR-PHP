5cc0c074d44f798ad38afff13a7f7768e0343b50
Mayur Pipaliya <mayurpipaliya@gmail.com>
2017-06-06 08:38:45 -0400

    initial commit

---
+++ map.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+
+ while($log = fgets(STDIN)){
+    preg_match('/^(\S+) (\S+) (\S+) \[([^:]+):(\d+:\d+:\d+) ([^\]]+)\] \"(\S+) (.*?) (\S+)\" (\S+) (\S+) (\".*?\") (\".*?\")$/',$log, $matches);
+    print_r($matches[10] . ' ' . $matches[8]  . "\n");
+} 
+
+?>
---
+++ reduce.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+ while($log = fgets(STDIN)){
+    $matches = explode(" ", $log,2);
+    $res[trim($matches[1])] = $matches[0];
+} 
+foreach($res as $key=>$value)
+    print_r("$value $key\n");
+
+?>
