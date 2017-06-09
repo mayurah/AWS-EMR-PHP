89dbdbd05c678b29c7c56e16e2aed81e9d92c45b
Mayur Pipaliya <mayurpipaliya@gmail.com>
2017-06-09 01:35:10 -0400

    fixes for map

--- mapr/reduce.php
+++ mapr/reduce.php
@@ -4,9 +4,11 @@
     $matches = explode(",", $log,2);    
     $res[trim($matches[1])] = $matches[0];
 } 
+$key=0;
 foreach($res as $key=>$value){
+ $key++; $unique = rand(0,21474836) + 1;
     if(trim($key) != null)
-      print_r("$value,$key\n");
+      print_r("$unique,$value,$key\n");
 }
 
 ?>
