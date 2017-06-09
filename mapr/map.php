4b9e118043e2f1b01acf506df5621a4f7891432c
Mayur Pipaliya <mayurpipaliya@gmail.com>
2017-06-08 23:54:36 -0400

    fixes for map

--- mapr/map.php
+++ mapr/map.php
@@ -3,7 +3,7 @@
 
  while($log = fgets(STDIN)){
     preg_match('/^(\S+) (\S+) (\S+) \[([^:]+):(\d+:\d+:\d+) ([^\]]+)\] \"(\S+) (.*?) (\S+)\" (\S+) (\S+) (\".*?\") (\".*?\")$/',$log, $matches);
-    print_r($matches[10] . ',' . $matches[8]  . "\n");
+    print_r(str_ireplace(',','',$matches[10]) . ',' . str_ireplace(',','',$matches[8])  . "\n");
 } 
 
 ?>
